c87556603fb125633a0db38946e47b001f1523db
rap9210 <rap9210@gmail.com>
2020-04-14 18:33:18 -0400

    SC Largest Number

--- src/LargestNumber.java
+++ src/LargestNumber.java
@@ -1,9 +1,7 @@
 import java.util.Scanner;
 public class LargestNumber {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int greatest = 0;
-        int smallest = 0;
         System.out.println("Enter 3 numbers to be compared.\n-");
         int num_1 = scn.nextInt();
         System.out.println("-");
@@ -11,33 +9,24 @@ public class LargestNumber {
         System.out.println("-");
         int num_3 = scn.nextInt();
 
-        if (num_1 > num_2){
-            if (num_1 > num_3){
-                greatest = num_1;
-                System.out.println("Greatest: "+greatest);
-            }
+        if ((num_1 > num_2)&&(num_1 > num_3)){
+            System.out.println("Greatest: "+num_1);
         }
-        if (num_2 > num_3){
-            greatest = num_2;
-            System.out.println("Greatest: "+greatest);
+        else if ((num_2 > num_1)&&(num_2 > num_3)){
+            System.out.println("Greatest: "+num_2);
         }
         else{
-            greatest = num_3;
-            System.out.println("Greatest: "+greatest);
+            System.out.println("Greatest: "+num_3);
         }
-        if (num_1 < num_2){
-            if (num_1 < num_3){
-                smallest = num_1;
-                System.out.println("Smallest: "+smallest);
-            }
+        if ((num_1 < num_2)&&(num_1 < num_3)){
+            System.out.println("Smallest: "+num_1);
         }
-        if (num_2 < num_3){
-            smallest = num_2;
-            System.out.println("Smallest: "+smallest);
+        else if ((num_2 < num_1)&&(num_2 < num_3)){
+            System.out.println("Smallest: "+num_2);
         }
         else{
-            smallest = num_3;
-            System.out.println("Smallest: "+smallest);
+            System.out.println("Smallest: "+num_3);
         }
     }
 }
+
